8084802878f93725504a3ca8f86e2979cc080ff2
Rohan D. Shah <rohan.shah3@aggiemail.usu.edu>
2014-05-29 22:33:15 -0600

    class containing bets
    c# class containing the bets of the players

---
+++ bettingParlor/bettingParlor/Bet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bettingParlor
+{
+    class Bet
+    {
+        public int amt;
+        public int dog;
+        public Guy bettor;
+
+        public string getDescription()
+        {
+            if (this.amt != 0)
+                return this.bettor.name + " bets " + this.amt + " on dog #" + this.dog;
+
+            else
+                return this.bettor.name + " hasn't placed a bet";
+        }
+
+        public int payout(int winner)
+        {
+            if (winner == this.dog)
+            {
+                //this.reset();
+                bettor.updateLabel();
+                return this.amt;
+            }
+            else
+            {
+                //this.reset();
+                bettor.updateLabel();
+                return -this.amt;
+            }
+
+           
+        }
+
+        public void reset()
+        {
+            this.amt = 0;
+            this.dog = 0;
+        }
+    }
+}
