2833e26222b320944df8f5b07753bc5712045eb6
Rohan D. Shah <rohan.shah3@aggiemail.usu.edu>
2014-05-29 22:53:15 -0600

    class contains the definition of dogs
    class of dogs that run the race

---
+++ bettingParlor/bettingParlor/Greyhound.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bettingParlor
+{
+    class Greyhound
+    {
+        public int startPos;
+        public int raceTrackLen;
+        public PictureBox myPicBox;
+        public int myLoc = 0;
+        public Random randomizer;
+
+        public bool run()
+        {
+            //startPos += myLoc;
+            myLoc += randomizer.Next(10) + 5;            
+            myPicBox.Left = startPos + myLoc ;
+
+            if (myLoc >= raceTrackLen)
+               return true;
+
+            return false;            
+        }
+
+        public void takeStartPos()
+        {
+            myLoc = 0;
+            myPicBox.Left = myLoc;
+        }
+    }
+}
